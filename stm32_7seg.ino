a0926d522adf62a056a6b477d6597bccde8a9271
Atul Kumar <78437677+9bits-Atulkumar@users.noreply.github.com>
2021-02-04 20:22:46 +0530

    Add files via upload

---
+++ stm32_7seg.ino
@@ -0,0 +1,169 @@
+
+void setup() {
+pinMode(PA0,OUTPUT);
+pinMode(PA1,OUTPUT);
+pinMode(PA2,OUTPUT);
+pinMode(PA3,OUTPUT);
+pinMode(PA4,OUTPUT);
+pinMode(PA5,OUTPUT);
+pinMode(PA6,OUTPUT);
+pinMode(PA7,OUTPUT);
+pinMode(PB0,OUTPUT);
+
+}
+
+void loop() {
+  //10
+
+ 
+  digitalWrite(PA0,LOW);
+  digitalWrite(PA1,LOW);
+  digitalWrite(PA2,LOW);
+  digitalWrite(PA3,LOW);
+  digitalWrite(PA4,LOW);
+  digitalWrite(PA5,LOW);
+  digitalWrite(PA6,HIGH);
+  digitalWrite(PA7,LOW);
+  digitalWrite(PB0,LOW);
+  delay(1000);
+ 
+   //9
+   digitalWrite(PA0,LOW);
+  digitalWrite(PA1,LOW);
+  digitalWrite(PA2,LOW);
+  digitalWrite(PA3,LOW);
+  digitalWrite(PA4,HIGH);
+  digitalWrite(PA5,LOW);
+  digitalWrite(PA6,LOW);
+   digitalWrite(PA7,HIGH);
+  digitalWrite(PB0,HIGH);
+   delay(1000);
+   
+   //8
+    
+   digitalWrite(PA0,LOW);
+  digitalWrite(PA1,LOW);
+  digitalWrite(PA2,LOW);
+  digitalWrite(PA3,LOW);
+  digitalWrite(PA4,LOW);
+  digitalWrite(PA5,LOW);
+  digitalWrite(PA6,LOW);
+   digitalWrite(PA7,HIGH);
+  digitalWrite(PB0,HIGH);
+   delay(1000);
+ 
+    
+   //7
+    
+   digitalWrite(PA0,LOW);
+  digitalWrite(PA1,LOW);
+  digitalWrite(PA2,LOW);
+  digitalWrite(PA3,HIGH);
+  digitalWrite(PA4,HIGH);
+  digitalWrite(PA5,HIGH);
+  digitalWrite(PA6,HIGH);
+   digitalWrite(PA7,HIGH);
+  digitalWrite(PB0,HIGH);
+   delay(1000);
+   
+   //6
+    
+   digitalWrite(PA0,LOW);
+  digitalWrite(PA1,HIGH);
+  digitalWrite(PA2,LOW);
+  digitalWrite(PA3,LOW);
+  digitalWrite(PA4,LOW);
+  digitalWrite(PA5,LOW);
+  digitalWrite(PA6,LOW);
+   digitalWrite(PA7,HIGH);
+  digitalWrite(PB0,HIGH);
+   delay(1000);
+   
+  //5
+  
+   digitalWrite(PA0,LOW);
+  digitalWrite(PA1,HIGH);
+  digitalWrite(PA2,LOW);
+  digitalWrite(PA3,LOW);
+  digitalWrite(PA4,HIGH);
+  digitalWrite(PA5,LOW);
+  digitalWrite(PA6,LOW);
+   digitalWrite(PA7,HIGH);
+  digitalWrite(PB0,HIGH);
+    delay(1000);
+
+   //4
+   
+   digitalWrite(PA0,HIGH);
+  digitalWrite(PA1,LOW);
+  digitalWrite(PA2,LOW);
+  digitalWrite(PA3,HIGH);
+  digitalWrite(PA4,HIGH);
+  digitalWrite(PA5,LOW);
+  digitalWrite(PA6,LOW);
+   digitalWrite(PA7,HIGH);
+  digitalWrite(PB0,HIGH);
+      delay(1000);
+   //3
+   
+   digitalWrite(PA0,LOW);
+  digitalWrite(PA1,LOW);
+  digitalWrite(PA2,LOW);
+  digitalWrite(PA3,LOW);
+  digitalWrite(PA4,HIGH);
+  digitalWrite(PA5,HIGH);
+  digitalWrite(PA6,LOW);
+   digitalWrite(PA7,HIGH);
+  digitalWrite(PB0,HIGH);
+     delay(1000);
+  //2
+  
+   digitalWrite(PA0,LOW);
+  digitalWrite(PA1,LOW);
+  digitalWrite(PA2,HIGH);
+  digitalWrite(PA3,LOW);
+  digitalWrite(PA4,LOW);
+  digitalWrite(PA5,HIGH);
+  digitalWrite(PA6,LOW);
+   digitalWrite(PA7,HIGH);
+  digitalWrite(PB0,HIGH);
+    delay(1000);
+  //1
+   
+   digitalWrite(PA0,HIGH);
+  digitalWrite(PA1,LOW);
+  digitalWrite(PA2,LOW);
+  digitalWrite(PA3,HIGH);
+  digitalWrite(PA4,HIGH);
+  digitalWrite(PA5,HIGH);
+  digitalWrite(PA6,HIGH);
+   digitalWrite(PA7,HIGH);
+  digitalWrite(PB0,HIGH);
+    delay(1000);
+  //0
+   
+  digitalWrite(PA0,LOW);
+  digitalWrite(PA1,LOW);
+  digitalWrite(PA2,LOW);
+  digitalWrite(PA3,LOW);
+  digitalWrite(PA4,LOW);
+  digitalWrite(PA5,LOW);
+  digitalWrite(PA6,HIGH);
+   digitalWrite(PA7,HIGH);
+  digitalWrite(PB0,HIGH);
+   delay(1000);
+   
+
+
+   digitalWrite(PA0,HIGH);
+  digitalWrite(PA1,HIGH);
+  digitalWrite(PA2,HIGH);
+  digitalWrite(PA3,HIGH);
+  digitalWrite(PA4,HIGH);
+  digitalWrite(PA5,HIGH);
+  digitalWrite(PA6,HIGH);
+   digitalWrite(PA7,HIGH);
+  digitalWrite(PB0,HIGH);
+   delay(1000);
+
+}
